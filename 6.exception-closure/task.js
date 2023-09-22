33840f6bb65d051094801808c3a09bab7649d854
Ivan Surovegin <givenrous@yandex.ru>
2023-09-22 15:03:18 +0300

    решение

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,53 @@
-﻿
+﻿function parseCount(input) {
+    let parsedInput = Number.parseFloat(input);
+    if (isNaN(parsedInput)) {
+        throw new Error('Невалидное значение');
+    }
+    return parsedInput;
+}
+
+function validateCount(input) {
+    try {
+        return parseCount(input);
+    }
+    catch(error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new Error ('Треугольник с такими сторонами не существует');
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        let halfP = this.perimeter / 2;
+        let area = Math.sqrt(halfP * (halfP - this.a) * (halfP - this.b) * (halfP - this.c));
+        return +area.toFixed(3);
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle (a, b, c);
+    }
+    catch (error) {
+        return {
+            get perimeter() {
+                return 'Ошибка! Треугольник не существует';
+            },
+            get area() {
+                return 'Ошибка! Треугольник не существует';
+            }
+        }
+    }
+}
